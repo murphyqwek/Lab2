d635cee1624d3ae3b3eb9959ef0a35a1283df4db
murphyqwek <qwekinaft@list.ru>
2025-10-06 19:32:31 +0300

    Create AreaCheckUtils for checking if dot hit the blue area

---
+++ src/main/java/org/example/lab2/utils/AreaCheckUtils.java
@@ -0,0 +1,43 @@
+package org.example.lab2.utils;
+
+public class AreaCheckUtils {
+    private final double x;
+    private final double y;
+    private final double r;
+
+    public AreaCheckUtils(double x, double y, double r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public boolean isHit() {
+        return checkSquare() && checkTriangle() && checkQuadrant();
+    }
+
+    private boolean checkQuadrant() {
+        return x * x + y * y <= r * r && x <= 0 && y <= 0;
+    }
+
+    private boolean checkSquare() {
+        return (x >= -r && x <= 0 && y >= 0 && y <= r);
+    }
+
+    private boolean checkTriangle() {
+        double ax = 0, ay = 0;
+        double bx = r/2, by = 0;
+        double cx = 0, cy = r/2;
+
+        double v1 = crossProduct(ax, ay, bx, by, x, y);
+        double v2 = crossProduct(bx, by, cx, cy, x, y);
+        double v3 = crossProduct(cx, cy, ax, ay, x, y);
+
+        return (v1 >= 0 && v2 >= 0 && v3 >= 0) || (v1 <= 0 && v2 <= 0 && v3 <= 0);
+    }
+
+    private double crossProduct(double ax, double ay,
+                                      double bx, double by,
+                                      double cx, double cy) {
+        return (bx - ax) * (cy - ay) - (by - ay) * (cx - ax);
+    }
+}

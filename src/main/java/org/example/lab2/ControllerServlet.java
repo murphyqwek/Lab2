4063fd1900121e2c4611aabd2441f7fcaf0c289f
murphyqwek <qwekinaft@list.ru>
2025-10-06 14:05:30 +0300

    Add ControllerServlet with user's values validation and routing
    ControllerServlet validates and forwards the request to AreaCheckerServlet with the values.

---
+++ src/main/java/org/example/lab2/ControllerServlet.java
@@ -0,0 +1,29 @@
+package org.example.lab2;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/controller")
+public class ControllerServlet extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        Validation validation = new Validation(req.getParameter("xvalue"), req.getParameter("yvalue"), req.getParameter("rvalue"));
+
+        ValidationResult result = validation.validateForAllNums();
+
+        if(!result.isValid()) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST, result.errorMsg());
+            return;
+        }
+
+        req.setAttribute("userValueBean", validation.getUserValueBean());
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/areaChecker");
+        dispatcher.forward(req, res);
+    }
+}
